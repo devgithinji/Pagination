af9e5f3369cbd2e84e19c2f15270a6f8fe179e0f
dennis <wakahiad@gmail.com>
2022-06-11 12:11:36 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Frontend Pagination
+
+Click [here](https://react-frontend-pagination-ke.netlify.app) to visit application
+
+Dennis Githinji 👍
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 10;
+    const pages = Math.ceil(followers.length / itemsPerPage);
+
+    return Array.from({length: pages}, (_, index) => {
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage)
+    });
+}
 
 export default paginate
